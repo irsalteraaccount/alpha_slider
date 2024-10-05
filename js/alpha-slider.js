d4c0c701178c030eeba38f4579fca3b3bdff712d
arslan <gulzararslan@gmail.com>
2024-10-06 04:17:34 +0500

    back and forward work done

--- js/alpha-slider.js
+++ js/alpha-slider.js
@@ -5,7 +5,8 @@ class NewWaySlider extends HTMLElement {
     this.slides = this.querySelectorAll('.nw_slide');
     this.nwLeft = 0;
     this.next = this.onNextSlide.bind(this);
-    this.addEventListener('click', this.next);
+    this.prev = this.onPrevSlide.bind(this);
+    this.addEventListener('click', this.prev);
   }
 
   onNextSlide(){
@@ -18,12 +19,40 @@ class NewWaySlider extends HTMLElement {
     if(nextSlide) nextSlide.classList.add('nw_slide--active');
     if(!nextSlide){
       const firstSlide = this.slides[0];
-      const lastSlide = this.slides[(this.slides.length - 1)];
       firstSlide.classList.add('nw_slide--active');
-      lastSlide.classList.remove('nw_slide--prev');
       this.nwLeft = 0;
       this.track.style.setProperty('--nw-left', (0 + 'px'));
     }
+  }
+
+  onPrevSlide(){
+    const currSlide = this.querySelector('.nw_slide--active');
+    const currSlideBounds = currSlide.getBoundingClientRect().width;
+    const prevSlide = currSlide.previousElementSibling;
+    if(!prevSlide){
+      const lastSlide = this.slides[(this.slides.length - 1)];
+      const lastBound = lastSlide.getBoundingClientRect().width;
+      lastSlide.classList.add('nw_slide--active');
+      const totalWidth = this.getLastCount();
+      this.nwLeft = totalWidth - lastBound;
+      this.track.style.setProperty('--nw-left', ('-' + this.nwLeft + 'px'));
+      const firstSlide = this.slides[0];
+      firstSlide.classList.remove('nw_slide--active');
+      return;
+    }
+    currSlide.classList.remove('nw_slide--active');
+    prevSlide.classList.add('nw_slide--active');
+    this.nwLeft -= currSlideBounds;
+    this.track.style.setProperty('--nw-left', ('-' + this.nwLeft + 'px'));
+  }
+
+  getLastCount(){
+    let width = 0;
+    this.slides.forEach(slide => {
+      const bounds = slide.getBoundingClientRect().width;
+      width += bounds;
+    });
+    return width;
   }
 }
 if(!customElements.get('nw-slider')) customElements.define('nw-slider', NewWaySlider);
