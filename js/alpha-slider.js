a29b87eb722c67d1da57cad9829e555d66fd1743
arslan <gulzararslan@gmail.com>
2024-10-06 03:46:54 +0500

    new slider implementation underway, using css most, yet main defining working done

--- js/alpha-slider.js
+++ js/alpha-slider.js
@@ -1,64 +1,29 @@
-class AlphaSlide extends HTMLElement {
+class NewWaySlider extends HTMLElement {
   constructor() {
     super();
+    this.track = this.querySelector('.nw_track');
+    this.slides = this.querySelectorAll('.nw_slide');
+    this.nwLeft = 0;
+    this.next = this.onNextSlide.bind(this);
+    this.addEventListener('click', this.next);
   }
-  static _gsi(parent) {
-    const obj = {};
-    const slides = Array.from(parent.querySelectorAll('alpha-slide'));
-    obj.currIndex = slides.map(slide => slide.classList.contains('alp-slide--active')).indexOf(true);
-    obj.nextIndex = (obj.currIndex + 1) % slides.length;
-    obj.prevIndex = (obj.currIndex - 1 + slides.length) % slides.length;
-    obj.length = slides.length;
-    return obj;
-  }
-  static _sas(parent, index){
-    const slides = Array.from(parent.querySelectorAll('alpha-slide'));
-    slides.forEach(slide => {
-      slide.classList.remove('alp-slide--active');
-    });
-    slides[index].classList.add('alp-slide--active');
-    return slides[index];
-  }
-}
-class AlphaArrow extends HTMLElement {
-  constructor() {
-    super();
-    this.event = this._ocl.bind(this);
-    this.button = this.querySelector('button');
-    this.button.addEventListener('click', this.event);
-  }
-  _ocl(event){
-    event.preventDefault();
-    this.slider = this.closest(this.dataset.slider);
-    this.slidesData = AlphaSlide._gsi(this.slider);
-    switch(this.dataset.arrow){
-      case 'next':
-        this._ns();
-        break;
-      default:
-        this._ps();
-        break;
-    }
-  }
-  _ps(){
-    const currSlide = AlphaSlide._sas(this.slider, this.slidesData.prevIndex);
-    this._nxts(currSlide, this.dataset.arrow);
-  }
-  _ns(){
-    const currSlide = AlphaSlide._sas(this.slider, this.slidesData.nextIndex);
-    this._nxts(currSlide, this.dataset.arrow);
-  }
-  _nxts(slide, direction){
-    const slides = Array.from(this.slider.querySelectorAll('alpha-slide'));
-    const slideBounds = slides[this.slidesData.currIndex].getBoundingClientRect();
-    switch(direction){
-      case 'prev':
-        slides[this.slidesData.prevIndex].style.marginLeft = `0px`;
-        break;
-      default:
-        slides[this.slidesData.currIndex].style.marginLeft = `-${slideBounds.width}px`;
+
+  onNextSlide(){
+    const currSlide = this.querySelector('.nw_slide--active');
+    const currSlideBounds = currSlide.getBoundingClientRect().width;
+    const nextSlide = currSlide.nextElementSibling;
+    currSlide.classList.remove('nw_slide--active');
+    this.nwLeft += currSlideBounds;
+    this.track.style.setProperty('--nw-left', ('-' + this.nwLeft + 'px'));
+    if(nextSlide) nextSlide.classList.add('nw_slide--active');
+    if(!nextSlide){
+      const firstSlide = this.slides[0];
+      const lastSlide = this.slides[(this.slides.length - 1)];
+      firstSlide.classList.add('nw_slide--active');
+      lastSlide.classList.remove('nw_slide--prev');
+      this.nwLeft = 0;
+      this.track.style.setProperty('--nw-left', (0 + 'px'));
     }
   }
 }
-if(!customElements.get('alpha-slide')) customElements.define('alpha-slide', AlphaSlide);
-if(!customElements.get('alpha-arrow')) customElements.define('alpha-arrow', AlphaArrow);
+if(!customElements.get('nw-slider')) customElements.define('nw-slider', NewWaySlider);
